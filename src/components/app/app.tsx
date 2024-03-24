cca20eccbec5418f96463ab6f100e5c7d5f33c97
Живов Алексей <aazhivov1@www.mts.ru>
2024-03-24 18:21:11 +0300

    предварительно сделан роутинг

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,12 +1,89 @@
+import {
+  ConstructorPage,
+  Feed,
+  ForgotPassword,
+  Login,
+  NotFound404,
+  Profile,
+  ProfileOrders,
+  Register,
+  ResetPassword
+} from '@pages';
 import '../../index.css';
 import styles from './app.module.css';
 
-import { AppHeader } from '@components';
+import { AppHeader, IngredientDetails, Modal, OrderInfo } from '@components';
+import {
+  BrowserRouter,
+  Route,
+  Router,
+  Routes,
+  useLocation,
+  useNavigate
+} from 'react-router-dom';
+
+const AppRouter = () => {
+  const location = useLocation();
+  const background = location.state?.background;
+  const navigate = useNavigate();
+
+  return (
+    <>
+      <Routes location={background || location}>
+        {/* <Route path='/' element={<Main />} /> */}
+        <Route path='/feed' element={<Feed />} />
+        <Route path='/feed/:number' element={<OrderInfo />} />
+        <Route path='/ingredients/:id' element={<IngredientDetails />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/register' element={<Register />} />
+        <Route path='/forgot-password' element={<ForgotPassword />} />
+        <Route path='/reset-password' element={<ResetPassword />} />
+        <Route path='/profile' element={<Profile />} />
+        <Route path='/profile/orders' element={<ProfileOrders />} />
+        <Route path='/profile/orders/:number' element={<OrderInfo />} />
+        <Route path='*' element={<NotFound404 />} />
+      </Routes>
+
+      {background && (
+        <Routes>
+          <Route
+            path='/feed/:number'
+            element={
+              <Modal title={'Информация о заказе'} onClose={() => navigate(-1)}>
+                <OrderInfo />
+              </Modal>
+            }
+          />
+          <Route
+            path='/ingredients/:id'
+            element={
+              <Modal title={'Детали ингредиента'} onClose={() => navigate(-1)}>
+                <IngredientDetails />
+              </Modal>
+            }
+          />
+          <Route
+            path='/profile/orders/:number'
+            element={
+              <Modal title={'Информация о заказе'} onClose={() => navigate(-1)}>
+                <OrderInfo />
+              </Modal>
+            }
+          />
+        </Routes>
+      )}
+    </>
+  );
+};
 
 const App = () => (
-  <div className={styles.app}>
-    <AppHeader />
-  </div>
+  <BrowserRouter>
+    <div className={styles.app}>
+      <AppHeader />
+      <ConstructorPage />
+      <AppRouter />
+    </div>
+  </BrowserRouter>
 );
 
 export default App;
