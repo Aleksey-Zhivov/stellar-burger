4482ca2ea89e5d8ea2a789819abe29b88a3fd28c
Живов Алексей <aazhivov1@www.mts.ru>
2024-04-17 12:19:56 +0300

    закончено написание теста на оформление и отправку заказа

--- cypress/e2e/constructor.cy.tsx
+++ cypress/e2e/constructor.cy.tsx
@@ -1,5 +1,9 @@
-describe('Проверка /api/ingredients', () => {
-  beforeEach(() => {
+import * as authTokens from '../fixtures/authTokens.json';
+import * as userData from '../fixtures/userData.json';
+import * as orderData from '../fixtures/order.json';
+
+describe('Проверка загрузки ингредиентов и добавления их в конструктор', () => {
+  before(() => {
     cy.intercept('GET', 'api/ingredients', { fixture: 'ingredients.json' });
     cy.visit('http://localhost:4000/');
   });
@@ -42,48 +46,108 @@ describe('Проверка /api/ingredients', () => {
       'Краторная булка N-200i (низ)'
     );
   });
+
+  describe('Проверка работы модальных окон на примере ингредиентов', () => {
+    beforeEach(() => {
+      cy.intercept('GET', 'api/ingredients', { fixture: 'ingredients.json' });
+      cy.visit('http://localhost:4000/');
+    });
+
+    it('Открытие модального окна с ингредиентом', () => {
+      const ingredient = cy.get(':nth-child(4) > :nth-child(5)');
+
+      ingredient.click();
+
+      const modal = cy.get('#modals > div:first-child');
+      const header = modal.get('div:first-child > h3');
+
+      header.contains('Хрустящие минеральные кольца');
+    });
+
+    it('Закрытие модального окна по клику на кнопку Закрыть (крестик)', () => {
+      const ingredient = cy.get(':nth-child(4) > :nth-child(5)');
+
+      ingredient.click();
+
+      const modal = cy.get('#modals > div:first-child').as('modal');
+      const button = modal.get('div:first-child > button > svg');
+
+      button.click();
+
+      cy.get('modal').should('not.exist');
+    });
+
+    it('Закрытие модального окна по клику на оверлей', () => {
+      const ingredient = cy.get(':nth-child(4) > :nth-child(5)');
+
+      ingredient.click();
+
+      const modal = cy.get('#modals > div:first-child').as('modal');
+      const overlay = modal.get('#modals > div:nth-child(2)');
+
+      overlay.click({ force: true });
+
+      cy.get('modal').should('not.exist');
+    });
+  });
 });
 
-describe('Проверка работы модальных окон на примере ингредиентов', () => {
+describe('Проверка создания заказа', () => {
   beforeEach(() => {
     cy.intercept('GET', 'api/ingredients', { fixture: 'ingredients.json' });
+    cy.intercept('GET', 'api/auth/user', { fixture: 'userData.json' });
+    cy.setCookie('accessToken', authTokens.accessToken);
+    localStorage.setItem('refreshToken', authTokens.refreshToken);
+    cy.intercept('GET', 'api/auth/tokens', {
+      fixture: 'authTokens.json'
+    });
+    cy.intercept('POST', 'api/orders', { fixture: 'order.json' });
     cy.visit('http://localhost:4000/');
   });
 
-  it('Открытие модального окна с ингредиентом', () => {
-    const ingredient = cy.get(':nth-child(4) > :nth-child(5)');
+  it('', () => {
+    const bun = cy.get(':nth-child(2) > :nth-child(1) > .common_button');
+    const main = cy.get(':nth-child(4) > :nth-child(3) > .common_button');
+    const sauce = cy.get(':nth-child(6) > .common_button');
 
-    ingredient.click();
+    bun.click();
+    main.click();
+    sauce.click();
 
-    const modal = cy.get('#modals > div:first-child');
-    const header = modal.get('div:first-child > h3');
+    const orderButton = cy.get(
+      '#root > div > main > div > section:nth-child(2) > div > button'
+    );
 
-    header.contains('Хрустящие минеральные кольца');
-  });
+    orderButton.click();
 
-  it('Закрытие модального окна по клику на кнопку Закрыть (крестик)', () => {
-    const ingredient = cy.get(':nth-child(4) > :nth-child(5)');
+    const orderModal = cy.get('#modals > div:first-child');
+    const orderNumber = orderModal.get('div:nth-child(2) > h2');
 
-    ingredient.click();
+    orderNumber.contains(orderData.order.number);
 
-    const modal = cy.get('#modals > div:first-child').as('modal');
-    const button = modal.get('div:first-child > button > svg');
+    const button = orderModal.get(
+      'div:first-child > div:first-child > button > svg'
+    );
 
     button.click();
 
     cy.get('modal').should('not.exist');
+
+    const burgerCunstructor = {
+      constructorBunTop: cy.get('div > section:nth-child(2) > div'),
+      constructoMainIngredient: cy.get('div > section:nth-child(2) > ul > div'),
+      constructorBunBottom: cy.get(
+        'div > section:nth-child(2) > div:nth-child(3)'
+      )
+    };
+
+    burgerCunstructor.constructorBunTop.contains('Выберите булки');
+    burgerCunstructor.constructoMainIngredient.contains('Выберите начинку');
+    burgerCunstructor.constructorBunBottom.contains('Выберите булки');
   });
 
-  it('Закрытие модального окна по клику на оверлей', () => {
-    const ingredient = cy.get(':nth-child(4) > :nth-child(5)');
-
-    ingredient.click();
-
-    const modal = cy.get('#modals > div:first-child').as('modal');
-    const overlay = modal.get('#modals > div:nth-child(2)');
-
-    overlay.click({ force: true });
-
-    cy.get('modal').should('not.exist');
+  afterEach(() => {
+    cy.clearAllCookies();
+    localStorage.removeItem('refreshToken');
   });
 });
--- src/utils/burger-api.ts
+++ src/utils/burger-api.ts
@@ -76,7 +76,6 @@ export const getIngredientsApi = () =>
     .then((res) => checkResponse<TIngredientsResponse>(res))
     .then((data) => {
       if (data?.success) {
-        console.log(data);
         return data.data;
       }
       return Promise.reject(data);
